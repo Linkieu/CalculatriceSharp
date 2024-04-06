fff00bcf5c41fb7077cfb4170ec8e6b51eac1ec2
Matthieu <106311363+Linkieu@users.noreply.github.com>
2024-04-06 23:39:52 +0200

    clic nb -> écrit sur l'afficheur, Videur OK + ajouts des boutons + - * / C ->

--- Calulatrice/MainWindow.xaml.cs
+++ Calulatrice/MainWindow.xaml.cs
@@ -19,10 +19,46 @@ namespace Calulatrice
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {        
+        string nombreEntree = "";
+        string operation = "C";
         public MainWindow()
         {
             InitializeComponent();
+            nouvelleOperation("C");
+        }
+
+        private void Chiffre_Click(object sender, RoutedEventArgs e)
+        {
+            // Convertir le sender en Button
+            if (sender is Button button)
+            {
+                // Ajoute à la suite des nombres entrés le contenu du bouton (chiffre)
+                string chiffre = button.Content.ToString();
+
+                if (! (chiffre == "0" && nombreEntree == "")){
+                    // Si on essaye pas d'ajouter de 0 dès le départ, quand nombreEntree est vide
+                    nombreEntree += chiffre;
+                    numberDisplay.Text = nombreEntree;
+                    nouvelleOperation("");
+                }
+
+            }
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e){
+            // Convertir le sender en Button
+            if (sender is Button button)
+            {
+                nombreEntree = "";
+                numberDisplay.Text = "0";
+                nouvelleOperation("C");
+            }
+        }
+
+        private void nouvelleOperation(string nOperation){
+            operation = nOperation;
+            if (operation == "C") { boutonReset.IsEnabled = false; } else { boutonReset.IsEnabled = true; }
         }
     }
 }
